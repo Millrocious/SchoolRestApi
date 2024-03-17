c3a3a93a50da11d7dddf9f7f024e8392052c9ec6
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2024-03-17 14:29:46 +0200

    Make all student fields private

--- src/main/java/com/endropioz/schoolrestapp/student/dto/StudentRequestDto.java
+++ src/main/java/com/endropioz/schoolrestapp/student/dto/StudentRequestDto.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class StudentRequestDto {
         @NotBlank(message = "First name is required")
         @ExcelCellName("firstName")
